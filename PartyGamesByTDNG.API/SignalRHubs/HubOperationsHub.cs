bffb8683a141d31e86237f01b35dd514a42550ac
fernandoangelesjr <fernandoangelesjr_02@live.com>
2024-05-02 22:42:03 +0800

    added JoinGame signalr method

--- PartyGamesByTDNG.API/SignalRHubs/HubOperationsHub.cs
+++ PartyGamesByTDNG.API/SignalRHubs/HubOperationsHub.cs
@@ -162,5 +162,103 @@ namespace PartyGamesByTDNG.API.SignalRHubs
         }
 
 
+
+
+
+
+
+        //operations by Game member
+        public async Task JoinGame(string Token, string RoomName, string PassCode, string GameCode, string PlayerName)
+        {
+            string return_method = "JoinGameResponse";
+
+            if (TokenHelper.IsTokenValid(Token))
+            {
+                var gamedata = _partygamesbytdng.HubGroups.Where(g => g.RoomName.ToUpper().Trim() == RoomName.ToUpper().Trim() && g.Passcode.Trim() == PassCode.Trim()).FirstOrDefault();
+
+                if (gamedata != null)
+                {
+                    if (gamedata.IsOpen == 1)
+                    {
+
+                        //add to members
+                        _partygamesbytdng.HubMembers.Add(new HubMember
+                        {
+                            ConnectionId = Context.ConnectionId,
+                            RoomCode = gamedata.RoomCode,
+                            LastActivity = DateTime.Now,
+                            Username = PlayerName.Trim()
+                        });
+
+                        gamedata.LastActivity = DateTime.Now;
+                        _partygamesbytdng.HubGroups.Update(gamedata);
+
+
+                        try
+                        {
+                            await _partygamesbytdng.SaveChangesAsync();
+
+                            await Groups.AddToGroupAsync(Context.ConnectionId, gamedata.RoomCode);
+
+                            await Clients.GroupExcept(gamedata.RoomName, Context.ConnectionId).SendAsync(return_method, ResponseBuilder.Build(new Response
+                            {
+                                Result = ResponseCode.Success,
+                                ResultTitle = "Success",
+                                ResultMessage = $"{PlayerName.Trim()} has joined the game",
+                                ResponseParams = $"{PlayerName.Trim()},{Context.ConnectionId}"
+                            }));
+
+                            await Clients.Client(Context.ConnectionId).SendAsync(return_method, ResponseBuilder.Build(new Response
+                            {
+                                Result = ResponseCode.Success,
+                                ResultTitle = "Success",
+                                ResultMessage = $"You have joined the game \"{gamedata.RoomName}\""
+                            }));
+
+                        }
+                        catch (Exception ex)
+                        {
+                            await Clients.Client(Context.ConnectionId).SendAsync(return_method, ResponseBuilder.Build(new Response
+                            {
+                                Result = ResponseCode.Failed,
+                                ResultTitle = "Error",
+                                ResultMessage = ex.Message
+                            }));
+                        }
+
+
+                    }
+                    else
+                    {
+                        await Clients.Client(Context.ConnectionId).SendAsync(return_method, ResponseBuilder.Build(new Response
+                        {
+                            Result = ResponseCode.Failed,
+                            ResultTitle = "Error",
+                            ResultMessage = "Sorry, the current game does not accept new players as of now."
+                        }));
+                    }
+                }
+                else
+                {
+                    await Clients.Client(Context.ConnectionId).SendAsync(return_method, ResponseBuilder.Build(new Response
+                    {
+                        Result = ResponseCode.Failed,
+                        ResultTitle = "Error",
+                        ResultMessage = "Invalid game credentials"
+                    }));
+                }
+            }
+            else
+            {
+                await Clients.Client(Context.ConnectionId).SendAsync(return_method, ResponseBuilder.Build(new Response
+                {
+                    Result = ResponseCode.Failed,
+                    ResultTitle = "Invalid token",
+                    ResultMessage = "Invalid token"
+                }));
+            }
+        }
+
+
     }
 }
