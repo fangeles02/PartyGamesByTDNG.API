c20325913400217e805690aea188e577705284b7
fernandoangelesjr <fernandoangelesjr_02@live.com>
2024-05-02 22:40:02 +0800

    added JWT validators and references

---
+++ PartyGamesByTDNG.API/Helper/TokenHelper.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PartyGamesByTDNG.API.Helper;
+
+public class TokenHelper
+{
+    public static bool IsTokenValid(string Token)
+    {
+
+        return Authenticate(Token);
+    }
+
+
+    private static bool Authenticate(string token)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Program.Key));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        List<Exception> validationFailures = null;
+        SecurityToken validatedToken;
+        var validator = new JwtSecurityTokenHandler();
+
+        // These need to match the values used to generate the token
+        TokenValidationParameters validationParameters = new TokenValidationParameters();
+        validationParameters.ValidateIssuer = true;
+        validationParameters.ValidateAudience = true;
+        validationParameters.ValidateLifetime = true;
+        validationParameters.ValidateIssuerSigningKey = true;
+
+        validationParameters.ValidIssuer = Program.Issuer;
+        validationParameters.ValidAudience = Program.Issuer;
+        validationParameters.IssuerSigningKey = key;
+
+
+        validationParameters.LifetimeValidator = (DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters) =>
+            {
+                //var a = options.ClaimsIssuer;
+                // string tokenstring = securityToken.UnsafeToString();
+                // var token = new JwtSecurityTokenHandler().ReadJwtToken(tokenstring);
+                // var claim = token.Claims.First(c => c.Type == "sample").Value;
+
+
+                // bool valid = Validators.ValidateLifetime()
+                // --> my custom check <--
+
+                var convnow = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now);
+
+
+
+                if (convnow >= (expires ?? DateTime.Now))
+                {
+                    return false;
+                }
+                else
+                {
+                    var timespan = (expires ?? DateTime.Now) - convnow;
+
+                    if (timespan.TotalSeconds <= 10)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+
+
+            };
+
+
+        if (validator.CanReadToken(token))
+        {
+            ClaimsPrincipal principal;
+            try
+            {
+                // This line throws if invalid
+                principal = validator.ValidateToken(token, validationParameters, out validatedToken);
+
+                // // If we got here then the token is valid
+                // if (principal.HasClaim(c => c.Type == ClaimTypes.Email))
+                // {
+                //     return principal.Claims.Where(c => c.Type == ClaimTypes.Email).First().Value;
+                // }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.InnerException);
+            }
+        }
+
+        return false;
+    }
+}
--- PartyGamesByTDNG.API/Program.cs
+++ PartyGamesByTDNG.API/Program.cs
@@ -3,6 +3,13 @@ using PartyGamesByTDNG.API;
 using PartyGamesByTDNG.API.DbContexts;
 using PartyGamesByTDNG.API.SignalRHubs;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+
+
 dbConnections environment = dbConnections.testserver;
 
 
@@ -29,6 +36,53 @@ builder.Services.AddCors(o => o.AddPolicy(
             .AllowAnyHeader()
     ));
 
+Issuer = builder.Configuration["Jwt:Issuer"];
+Key = builder.Configuration["Jwt:Key"];
+
+//adding jwt auth
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            //define which claim requires to check
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            //store the value in appsettings.json
+            LifetimeValidator = (DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters) =>
+            {
+                var a = options.ClaimsIssuer;
+                // string tokenstring = securityToken.UnsafeToString();
+                // var token = new JwtSecurityTokenHandler().ReadJwtToken(tokenstring);
+                // var claim = token.Claims.First(c => c.Type == "sample").Value;
+
+
+                // bool valid = Validators.ValidateLifetime()
+                // --> my custom check <--
+
+                var convnow = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now);
+
+                if (convnow >= (expires ?? DateTime.Now))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+
+            },
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+
 
 var app = builder.Build();
 
@@ -53,3 +107,10 @@ app.UseCors("CorsPolicy");
 
 
 app.Run();
+
+
+public partial class Program
+{
+    public static string Issuer;
+    public static string Key;
+}
